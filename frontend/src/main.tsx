26a89071b04200da5c66e07bcda7d8ebecf7e9e6
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-02-13 11:29:45 +0100

    merge de la dev

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,14 +8,7 @@ const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-=======
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 );
->>>>>>> 432cea1 (initialisation tailwind)
