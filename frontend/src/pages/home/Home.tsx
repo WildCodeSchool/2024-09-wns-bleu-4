b876336e66ea3cb2eb2a17b793fa24ae7aca7055
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-02-14 10:22:12 +0100

    fix des erreurs de plugins tailwind

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
     return (
-        <h1 className="home-title">
-            La solution simple et rapide pour tous vos transferts de fichiers
-        </h1>
+        <>
+            <h1 className="home-title">
+                La solution simple et rapide pour tous vos transferts de fichiers
+            </h1>
+        </>
     );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,5 @@ export default defineConfig({
         hmr: { path: '/hmr' },
         allowedHosts: ['frontend'],
     },
+    css: { postcss: { plugins: [tailwindcss()] } }
 });
