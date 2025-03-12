9a12a322dd9048727f38c0619f667ef8c4e50d1f
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-03-12 14:45:26 +0100

    mise en place de codegen et de la la génération de hooks

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,17 +1,19 @@
-import { CodegenConfig } from "@graphql-codegen/cli";
-
+import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
-    schema: "http://backend:4000",
-    documents: ["src/**/*.tsx"],
-    generates: {
-      "./src/__generated__/": {
-        preset: "client",
-        presetConfig: {
-          gqlTagName: "gql",
-        },
+  overwrite: true,
+  schema: "http://backend:4000/",
+  documents: ["src/graphql/*.ts"],
+  generates: {
+    "./src/generated/graphql-types.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+      config: {
+        withHooks: true,
       },
     },
-    ignoreNoDocuments: true,
-  };
-
+  },
+};
 export default config;
