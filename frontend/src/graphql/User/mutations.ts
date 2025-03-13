1218dd0e21bae8dddfee94ca02ee5788012a9d07
Jeynox <daniel.lemos.jose1998@gmail.com>
2025-03-13 15:08:58 +0100

    merge

--- frontend/src/graphql/User/mutations.ts
+++ frontend/src/graphql/User/mutations.ts
@@ -1,12 +1,22 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
+
+//Login
 export const LOGIN = gql`
     mutation Login($data: UserInput!) {
         login(data: $data)
     }
 `;
 
+//Register
 export const REGISTER = gql`
     mutation Register($data: UserInput!) {
         register(data: $data)
+    }
+`;
+
+//Confirm email
+export const CONFIRM_EMAIL = gql`
+    mutation ConfirmEmail($codeByUser: String!) {
+        confirmEmail(codeByUser: $codeByUser)
     }
 `;
