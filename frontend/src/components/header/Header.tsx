b5aad8c4ae9fbb44262f6600c95a66a39136f970
niieeer <clement0201@icloud.com>
2025-06-11 14:32:44 +0200

    refactor: reorganize imports in Header and Button components for consistency

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import Logo from '@/components/Logo';
 import { ModeToggle } from '@/components/mode-toggle';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 import {
     Menubar,
     MenubarContent,
@@ -8,14 +9,13 @@ import {
     MenubarMenu,
     MenubarSeparator,
 } from '@/components/ui/menubar';
+import { NavigationMenu } from '@/components/ui/navigation-menu';
 import { useAuthContext } from '@/context/useAuthContext';
 import { useLogoutMutation } from '@/generated/graphql-types';
 import { MenubarTrigger } from '@radix-ui/react-menubar';
 import { UserIcon } from 'lucide-react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { Button } from '../ui/button';
-import { NavigationMenu } from '../ui/navigation-menu';
 
 const Header = () => {
     const [logout] = useLogoutMutation();
