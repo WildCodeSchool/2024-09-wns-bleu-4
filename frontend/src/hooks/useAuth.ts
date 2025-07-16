bb9659d4cfedca466c8c4ed2d38b4e7244e9a40c
Etimo <gitimo@gmail.com>
2025-07-16 15:54:46 +0200

    Fixed AdminRoute et Refacto

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useAuthContext } from '../context/useAuthContext';
 
 export const useAuth = () => {
-    const { user, isAuth, loading } = useAuthContext();
-    return { user, isAuth, loading };
+    const { user, isAuth, loading, refreshAuth } = useAuthContext();
+    return { user, isAuth, loading, refreshAuth };
 };
