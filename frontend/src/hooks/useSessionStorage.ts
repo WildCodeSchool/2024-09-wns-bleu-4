8eaa969906f115d495abe74dd41d758256972696
Etimo <gitimo@gmail.com>
2025-07-17 17:01:58 +0200

    Fixed useLocalStorage and useSessionStorage

--- frontend/src/hooks/useSessionStorage.ts
+++ frontend/src/hooks/useSessionStorage.ts
@@ -1,11 +1,7 @@
-import { useState } from "react";
-
 export const useSessionStorage = () => {
-  const [value, setValue] = useState<string | null>(null);
 
   const setItem = (key: string, value: string) => {
     sessionStorage.setItem(key, value);
-    setValue(value);
   };
 
   const getItem = (key: string) => {
