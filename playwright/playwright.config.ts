aa382a18a0d2842e61341fc697e38a7ac6e6cf15
Etimo <gitimo@gmail.com>
2025-07-18 15:03:26 +0200

    Testing on 8

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -19,9 +19,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.RETRIES ? parseInt(process.env.RETRIES) : 0,
+  retries: process.env.RETRIES ? parseInt(process.env.RETRIES) : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 1,
+  workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
